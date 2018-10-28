64767ebaff21dc98107716f3a9f834f1c7943053
jweboy <1259524459@qq.com>
2018-10-28 23:14:43 +0800

    Add auto install script

--- bin/jweboy.js
+++ bin/jweboy.js
@@ -2,15 +2,21 @@
 
 const program = require('commander')
 const init = require('../command/init')
+const version = require('../package').version
 
 program
-  .version(require('../package').version)
+  .version(version)
   .usage('<command> [option]')
 
 program
   .command('init <project-name>')
-  .description('generate a project from remote template.')
+  .description('generate a react-webpack-template  from remote github.')
   .action(init)
 
 // 这句是关键,用来将Program写入到命令行
 program.parse(process.argv)
+
+// 不带任何参数的默认命令会显示help信息
+if(!program.args.length) {
+  program.help()
+}
