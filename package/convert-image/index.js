55e842d8953c4fe7beb17e43136d72c96d9dcb42
jweboy <1259524459@qq.com>
2019-06-05 22:10:21 +0800

    :construction: Optimize covert image function

--- package/convert-image/index.js
+++ package/convert-image/index.js
@@ -1,15 +1,16 @@
-const fs = require("fs");
-const { promisify } = require("util");
-const path = require("path");
-const gm = require("gm");
-const inquirer = require("inquirer");
+const fs = require('fs');
+const { promisify } = require('util');
+const path = require('path');
+const gm = require('gm');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
 
 const readdirAsync = promisify(fs.readdir);
 const mkdirAsync = promisify(fs.mkdir);
 const statAsync = promisify(fs.stat);
 
-const { rootPath } = require("../../config/paths");
-const questions = require("../../prompt/convert");
+const { rootPath } = require('../../config/paths');
+const questions = require('../../prompt/convert');
 
 /**
  * 将 SVG 图片转换为指定格式的图片并输出到指定目录
@@ -18,41 +19,33 @@ const questions = require("../../prompt/convert");
  * @param {String} suffix 图片后缀名
  */
 async function covert(outDir, suffix) {
-  // get svg files
-  const files = await readdirAsync(rootPath);
-  const svgFiles = files.filter(name => /.svg$/.test(name));
+  // svg suffix
+  const SVG_SUFFIX = '.svg';
 
-  // get image files
-  const imgFiles = svgFiles.reduce((arr, name) => {
-    const [fileName] = name.split(".");
-    const filePath = path.join(outDir, `${fileName}${suffix}`);
-    arr.push(filePath);
-    return arr;
-  }, []);
-  console.table(imgFiles);
+  // get svg files
+  const fileNames = await readdirAsync(rootPath);
+  const imageFiles = fileNames
+    .filter((name) => /.svg$/.test(name))
+    .map((name) => {
+      const [fileName] = name.split('.');
+      const sourcePath = path.join(rootPath, `${fileName}${SVG_SUFFIX}`);
+      const filePath = path.join(outDir, `${fileName}.${suffix.toLowerCase()}`);
+
+      return { sourceFile: sourcePath, targetFile: filePath };
+    });
 
   /* eslint-disable */
-  // create images
-  for (const image of imgFiles) {
-    await fs.writeFileSync(image);
-  }
-
-  // covert svg to image
-  for (const svgFile of svgFiles) {
-    const [fileName] = svgFile.split(".");
-    const inputFile = path.join(rootPath, svgFile);
-    const outFile = path.join(outDir, `${fileName}${suffix}`);
-
-    await gm(inputFile)
-      .write(outFile, (err) => {
-        if(err) {
-          throw err
+  // create image file
+  for (const {sourceFile, targetFile} of imageFiles) {
+    await fs.writeFileSync(targetFile);
+    await gm(sourceFile).write(targetFile, (err) => {
+        if(!err) {
+          console.log(chalk.cyan(sourceFile), chalk.green(`has been converted successfully!`))
         }
-      })
+    })
   }
 };
 
-// TODO: 代码优化
 // TODO: 多线程优化
 module.exports = async function convertSVGToImage() {
   const { directory, suffix } = await inquirer.prompt(questions);
--- package/template/index.js
+++ package/template/index.js
@@ -1,17 +1,15 @@
-const fs = require("fs");
-const path = require("path");
-const util = require("util");
-const inquirer = require("inquirer");
-const chalk = require("chalk");
-const questions = require("../../prompt/prompt");
-const donwloadGithubFiles = require("../../utils/download-github-files");
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const questions = require('../../prompt/prompt');
+const donwloadGithubFiles = require('../../utils/download-github-files');
 
 const statAsync = util.promisify(fs.stat);
 
 module.exports = async function initTempalte() {
-  const { projectName, tempalteName, packageManager } = await inquirer.prompt(
-    questions
-  );
+  const { projectName, tempalteName, packageManager } = await inquirer.prompt(questions);
 
   // 项目名为空则使用默认名称
   const appName = projectName || tempalteName;
@@ -21,19 +19,15 @@ module.exports = async function initTempalte() {
   try {
     const stat = await statAsync(appFullPath);
     if (stat.isDirectory()) {
-      console.log(chalk.red("Current directory already exists.\n"));
-      console.log(
-        chalk.red(
-          "Either try using a new directory name, or remove the files listed above."
-        )
-      );
+      console.log(chalk.red('Current directory already exists.\n'));
+      console.log(chalk.red('Either try using a new directory name, or remove the files listed above.'));
     }
   } catch (err) {
     donwloadGithubFiles({
       tempalteName,
       projectName,
       appFullPath,
-      packageManager
+      packageManager,
     });
   }
 };
